89be42e4b3d4feaf61881b0adcc2853049041ce1
emar2022 <erik.agudelo@wearedev.co>
2023-08-03 23:58:59 -0500

    actualizacion de paquetes para la confirmacion de la reserva v4

--- src/main/java/com/ericagudelo/tasks/Search.java
+++ src/main/java/com/ericagudelo/tasks/Search.java
@@ -5,12 +5,15 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.Wait;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static com.ericagudelo.userinterface.DestinationPage.LBL_CITY;
+import static com.ericagudelo.userinterface.InfoPersonPage.FIRST_NAME;
 import static com.ericagudelo.userinterface.SearchPage.*;
 import static com.ericagudelo.util.ConstantManager.DESTINATION_ERROR_MESSAGE;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class Search implements Task {
@@ -34,6 +37,7 @@ public class Search implements Task {
                     Click.on(LBL_CITY.of(destination))
             );
         }else {
+            WaitUntil.the(DESTINATION_ERROR,isEnabled()).forNoMoreThan(5).seconds();
             DESTINATION_ERROR_MESSAGE = DESTINATION_ERROR.resolveFor(actor).getText().trim();
         }
     }
